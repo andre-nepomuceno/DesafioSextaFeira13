e17c3f45f2af58364eeffa0d87bc43646d7a9917
Andre Nepomuceno <andre.nepa@gmail.com>
2022-05-14 07:38:45 -0300

    Commit inicial, apenas com os arquivos do projeto base criado, e adicionando a Tabbar e as 5 ViewControllers que existirão como menu inicial.

---
+++ DesafioSextaFeira13/BuscaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BuscaViewController.swift
+//  DesafioSextaFeira13
+//
+//  Created by André N. dos Santos on 14/05/22.
+//
+
+import UIKit
+
+class BuscaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DesafioSextaFeira13/ConfiguracoesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ConfiguracoesViewController.swift
+//  DesafioSextaFeira13
+//
+//  Created by André N. dos Santos on 14/05/22.
+//
+
+import UIKit
+
+class ConfiguracoesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
